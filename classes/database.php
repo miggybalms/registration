54a6e57fa21bcb545907db150634dc5cfa0330a1
miggybalms <balmescl@students.nu-lipa.edu.ph>
2025-05-06 16:44:27 +0800

    admin regestration

---
+++ classes/database.php
@@ -0,0 +1,34 @@
+<?php
+ 
+class database{
+ 
+    function opencon(): PDO{
+ 
+return new PDO(
+ 
+    dsn: 'mysql:host=localhost;
+    dbname=dbs_name',
+    username: 'root',
+    password: '');
+ 
+    }
+    function signupUser($firstname, $lastname, $username, $password){
+        $con = $this->opencon();
+        try{
+            $con->beginTransaction();
+            $stmt = $con->prepare("INSERT INTO Admin(admin_FN, admin_LN, admin_username, admin_password) VALUES (?, ?, ?, ?)");
+            $stmt->execute([$firstname, $lastname, $username, $password]);
+            $userId = $con->lastInsertId();
+ 
+            $con->commit();
+            return $userId;
+ 
+ 
+        }catch(PDOException $e){
+            $con->rollBack();
+            return false;
+ 
+        }
+    }
+}
+ 
